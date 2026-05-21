2a017be1bee8a5f0732e46276e7666e7a5e744b2
Luc Ma <onion0709@gmail.com>
2026-05-21 12:22:31 +0800

    drm/bridge: adv7511: rename drm_atomic_state to drm_atomic_commit
    Catch up with the commit 5164f7e7ff8e ("drm: Rename struct drm_atomic_state to drm_atomic_commit")
    
    Signed-off-by: Luc Ma <onion0709@gmail.com>

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -786,7 +786,7 @@ static const struct adv7511 *bridge_to_adv7511_const(const struct drm_bridge *br
 }
 
 static void adv7511_bridge_atomic_pre_enable(struct drm_bridge *bridge,
-					 struct drm_atomic_state *state)
+					 struct drm_atomic_commit *state)
 {
 	struct adv7511 *adv = bridge_to_adv7511(bridge);
 
@@ -832,7 +832,7 @@ static void adv7511_bridge_atomic_disable(struct drm_bridge *bridge,
 }
 
 static void adv7511_bridge_atomic_post_disable(struct drm_bridge *bridge,
-					  struct drm_atomic_state *state)
+					  struct drm_atomic_commit *state)
 {
 	struct adv7511 *adv = bridge_to_adv7511(bridge);
 
